5a98bb9146e624abd92d05aaaaad9cb122be2d0d
EbukaMoneme <camoneme@gmail.com>
2021-09-13 20:49:13 +0000

    Created a custom, colorful and verbose version of console.assert for testing

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// Function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Apples", "Apples");
+assertEqual("4561asf", "abc");
+assertEqual(1.0, 1.0);
+assertEqual(123, 120);

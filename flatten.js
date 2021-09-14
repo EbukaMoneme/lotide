8614a0dd9fe4647f6bd06ba96bfaf06264476c5a
EbukaMoneme <camoneme@gmail.com>
2021-09-14 22:38:46 +0000

    Created a function to flatten nested arrays

---
+++ flatten.js
@@ -0,0 +1,35 @@
+// Check if arrays are equal
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Print result to console
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function(arr) {
+  let result = [];
+  for (let val of arr) {
+    if (Array.isArray(val)) {
+      for (let piece of val) {
+        result.push(piece);
+      }
+    } else {
+      result.push(val);
+    }
+  }
+  return result;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])
+assertArraysEqual(flatten([1, 2, [3, 4, 5], [6]]), [1, 2, 3, 4, 5, 6])

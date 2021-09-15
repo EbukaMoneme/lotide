6fbad47555d4a4d6ca0c75e1b3d25b1137111f0e
EbukaMoneme <camoneme@gmail.com>
2021-09-15 17:34:21 +0000

    Updated and linted code for countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -13,9 +13,9 @@ const countOnly = function(allItems, itemsToCount) {
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
-      results[item] += 1;
+        results[item] += 1;
       } else {
-      results[item] = 1;
+        results[item] = 1;
       }
     }
   }

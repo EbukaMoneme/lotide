70b82df5e5ea32b277dbee0b81b3228f6355468b
EbukaMoneme <camoneme@gmail.com>
2021-09-15 17:54:52 +0000

    Created a program to track the position of letters in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+// Check if arrays are equal
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Print result to console
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i]) {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+// Tests
+let result1 = letterPositions('hello');
+
+assertArraysEqual(result1['l'], [2, 3]);
+assertArraysEqual(result1['e'], [1]);
+
+let result2 = letterPositions('lighthouse in the house');
+assertArraysEqual(result2['l'], [0]);
+assertArraysEqual(result2['h'], [3, 5, 15, 18]);
+assertArraysEqual(result2['e'], [9, 16, 22]);

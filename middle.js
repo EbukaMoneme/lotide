a0a208b65e1ebae90fa5cbf2b820deda7db7fe12
EbukaMoneme <camoneme@gmail.com>
2021-09-15 13:50:39 +0000

    Developed a program to find the middle of an array

---
+++ middle.js
@@ -0,0 +1,40 @@
+// compare arrays
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Print result to console
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(arr) {
+  let midIndex;
+  if (arr.length <= 2) {
+    return [];
+  } else if (arr.length % 2 !== 0) {
+    midIndex = Math.ceil(arr.length / 2);
+    return arr[midIndex];
+  } else {
+    midIndex = (arr.length / 2) - 1;
+    let ans = arr.slice(midIndex, midIndex + 2);
+    return ans;
+  }
+};
+
+// Tests
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);

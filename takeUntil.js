2c1b6b92a9a2965edef7b44f42212b8b34c576e8
EbukaMoneme <camoneme@gmail.com>
2021-09-16 16:31:42 +0000

    Created a program to slice an array until a specific condition is no longer met; using callback functions

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+// Check if arrays are equal
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Print result to console
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(arr, callback) {
+  let i = 0;
+  while (!callback(arr[i])) {
+    i++;
+  }
+  return arr.slice(0, i);
+};
+
+// Tests
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+
+const data3 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results3 = takeUntil(data3, x => x.length === 2);
+assertArraysEqual(results3, [ 'I\'ve', 'been']);

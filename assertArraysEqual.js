52d8e64f34c1758a2d7ec2ace06f98b27983e57b
EbukaMoneme <camoneme@gmail.com>
2021-09-20 14:15:22 +0000

    modularized assertArraysEqual.js file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,5 @@
 // Check if arrays are equal
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 // Print result to console
 const assertArraysEqual = function(actual, expected) {
@@ -19,3 +9,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+module.exports = assertArraysEqual;
